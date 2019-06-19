cdabfd73d93471f565292fbf4e7663a7ee5dc091
huhx <gohuhx@163.com>
2019-06-19 09:22:46 +0800

    refactor: ship don't maintain direction, move it in move method

--- src/main/java/com/thoughtworks/huhx/Ship.java
+++ src/main/java/com/thoughtworks/huhx/Ship.java
@@ -4,18 +4,15 @@ public class Ship {
 
   private Location location;
 
-  private Direction direction;
-
   public Location getLocation() {
     return location;
   }
 
-  public Ship(Location location, Direction direction) {
+  public Ship(Location location) {
     this.location = location;
-    this.direction = direction;
   }
 
-  public Location move(int miles) {
+  public Location move(Direction direction, int miles) {
     Point point = location.getPoint();
     int x = point.getX() + direction.getX() * miles;
     int y = point.getY() + direction.getY() * miles;
--- src/test/java/com/thoughtworks/huhx/ShipTest.java
+++ src/test/java/com/thoughtworks/huhx/ShipTest.java
@@ -13,38 +13,38 @@ public class ShipTest {
   @Test
   public void whenInstantiatedThenLocationIsSet() {
     Location location = new Location(new Point(5, 7));
-    Ship ship = new Ship(location, WEST);
+    Ship ship = new Ship(location);
     assertEquals(location, ship.getLocation());
   }
 
   @Test
   public void whenShipMoveForward() {
-    Ship ship = new Ship(new Location(new Point(5, 7)), NORTH);
-    Location location = ship.move(1);
+    Ship ship = new Ship(new Location(new Point(5, 7)));
+    Location location = ship.move(NORTH, 1);
     assertEquals(location.getPoint().getX(), 5);
     assertEquals(location.getPoint().getY(), 8);
   }
 
   @Test
   public void whenShipMoveBackward() {
-    Ship ship = new Ship(new Location(new Point(5, 7)), SOUTH);
-    Location location = ship.move(1);
+    Ship ship = new Ship(new Location(new Point(5, 7)));
+    Location location = ship.move(SOUTH, 1);
     assertEquals(location.getPoint().getX(), 5);
     assertEquals(location.getPoint().getY(), 6);
   }
 
   @Test
   public void whenShipMoveLeft() {
-    Ship ship = new Ship(new Location(new Point(5, 7)), WEST);
-    Location location = ship.move(1);
+    Ship ship = new Ship(new Location(new Point(5, 7)));
+    Location location = ship.move(WEST, 1);
     assertEquals(location.getPoint().getX(), 4);
     assertEquals(location.getPoint().getY(), 7);
   }
 
   @Test
   public void whenShipMoveRight() {
-    Ship ship = new Ship(new Location(new Point(5, 7)), EAST);
-    Location location = ship.move(1);
+    Ship ship = new Ship(new Location(new Point(5, 7)));
+    Location location = ship.move(EAST, 1);
     assertEquals(location.getPoint().getX(), 6);
     assertEquals(location.getPoint().getY(), 7);
   }
